dbb1750b43c62712dce5b4ffa33365c8c98a7524
Srijan <79041986+Srijan-bot@users.noreply.github.com>
2023-11-06 13:08:31 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./styles.css";
 import Navbar from "./Navbar";
 import About from "./pages/About";
 import Event from "./pages/Event";
+
 export default function App() {
   console.log(window.location);
   let Component;
+
   switch (window.location.pathname) {
     case "/":
       Component = App;
@@ -15,12 +18,14 @@ export default function App() {
     case "/event":
       Component = Event;
       break;
+    default:
+      Component = App; // Set a default component to render if the path doesn't match any of the cases.
   }
+
   return (
     <>
       <Navbar />
-      {/* {Component} */}
-      <Component />
+      {Component && <Component />}
     </>
   );
 }
